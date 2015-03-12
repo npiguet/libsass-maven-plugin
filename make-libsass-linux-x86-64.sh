6072b86b2cab49ba15a20562e584fe0183e6a7fc
Nicolas Piguet <nicolas.piguet@swissquote.ch>
2015-03-12 11:09:46 +0100

    attempt to link libsass.so to earlier versions of libstcc++

--- make-libsass-linux-x86-64.sh
+++ make-libsass-linux-x86-64.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd src;
-BUILD="shared" make -C libsass;
+BUILD="shared" make -C libsass CXX=g++-4.6 CC=gcc-4.4;
 cp libsass/lib/libsass.so main/resources/linux-x86-64
 cd ..;
